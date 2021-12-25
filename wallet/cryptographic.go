194b1622be5dc4dd5d9f5d211751dd3c1d6ce6ee
freddie <freddieodonnell@gmail.com>
2021-12-25 16:47:34 +0200

    update cryptographic

--- wallet/cryptographic.go
+++ wallet/cryptographic.go
@@ -1,7 +1,6 @@
 package wallet
 
 import (
-	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/md5"
@@ -32,7 +31,9 @@ func (c *Cryptographic) GenerateKeyPair() {
 		fmt.Printf("Cannot generate RSA key\n")
 		os.Exit(1)
 	}
+
 	c.PrivateKeyObject = privatekey
+
 	publickey := &privatekey.PublicKey
 	c.PublicKeyObject = publickey
 
@@ -47,25 +48,8 @@ func (c *Cryptographic) GenerateKeyPair() {
 		Bytes: privateKeyBytes,
 	}
 
-	var b []byte
-	buffer := bytes.NewBuffer(b)
-
-	err = pem.Encode(buffer, privateKeyBlock)
-	if err != nil {
-		fmt.Printf("error when encode private pem: %s \n", err)
-		os.Exit(1)
-	}
-
-	sk := make([]byte, 2048)
-
-	_, err = buffer.Read(sk)
-	if err != nil {
-		panic(err)
-	}
-
-	c.PrivateKey = sk
-
-	buffer.Reset()
+	pemEncodedPrivKey := pem.EncodeToMemory(privateKeyBlock)
+	c.PrivateKey = pemEncodedPrivKey
 
 	// public key
 	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publickey)
@@ -78,20 +62,8 @@ func (c *Cryptographic) GenerateKeyPair() {
 		Bytes: publicKeyBytes,
 	}
 
-	err = pem.Encode(buffer, publicKeyBlock)
-	if err != nil {
-		fmt.Printf("error when encode private pem: %s \n", err)
-		os.Exit(1)
-	}
-
-	pk := make([]byte, 418)
-
-	_, err = buffer.Read(pk)
-	if err != nil {
-		panic(err)
-	}
-
-	c.PublicKey = pk
+	pemEncodedPubKey := pem.EncodeToMemory(publicKeyBlock)
+	c.PublicKey = pemEncodedPubKey
 }
 
 func (c *Cryptographic) GenerateSignature(message []byte) []byte {
@@ -116,7 +88,10 @@ func VerifySignature(signature []byte, publicKey []byte, message []byte) error {
 		return fmt.Errorf("error verifying signature: could not parse public key %+v", err)
 	}
 
-	pubKey := genericPublicKey.(*ecdsa.PublicKey)
+	pubKey, ok := genericPublicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return fmt.Errorf("could not cast pubKey to ecdsa pubKey")
+	}
 
 	msgHashSum := hashMessage(message)
 
